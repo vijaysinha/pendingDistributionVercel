b366b3649148f890d6fcbafde900e23dacb5c20e
Vijay Sinha <vijaysinha60@gmail.com>
2026-05-08 17:27:17 +0530

    Update PortComponent.jsx
    Updating the member in the UI.

--- src/components/PortComponent.jsx
+++ src/components/PortComponent.jsx
@@ -1,12 +1,12 @@
 import usePortData from "../usePortData";
 function PortComponent({ rc_id }) {
    
-    const { error, fps_id, loading } = usePortData(rc_id);
+    const { error, fps_id, loading, memberName } = usePortData(rc_id);
     console.log(fps_id)
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error fetching data</p>;
-    return <p className={` ${fps_id ? 'bg-red-400 text-zinc-300 px-2 rounded inline p-2' : 'bg-green-300 text-zinc-900 font-bold px-4 rounded inline p-2'}`}>
-        {fps_id ? `चावल का ${fps_id} से उठाव  ` : 'शायद उठाव शेष'}
-    </p>;
+    return (<p className={` ${fps_id ? 'bg-red-400 text-zinc-300 px-2 rounded inline p-2' : 'bg-green-300 text-zinc-900 font-bold px-4 rounded inline p-2 text-sm'}`}>
+        {fps_id ? `चावल का ${fps_id} से उठाव ` : 'शायद उठाव शेष '} {memberName}
+    </p>)
 }
 export default PortComponent;
