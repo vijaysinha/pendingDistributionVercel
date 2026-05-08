e778d48d6b3995746bb417fe09fa2ad1e54bd099
Vijay Sinha <vijaysinha60@gmail.com>
2026-05-08 16:50:45 +0530

    Update PortComponent.jsx
    Updating PortComponent description.

--- src/components/PortComponent.jsx
+++ src/components/PortComponent.jsx
@@ -6,7 +6,7 @@ function PortComponent({ rc_id }) {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error fetching data</p>;
     return <p className={` ${fps_id ? 'bg-red-400 text-zinc-300 px-2 rounded inline p-2' : 'bg-green-300 text-zinc-900 font-bold px-4 rounded inline p-2'}`}>
-        {fps_id ? `${fps_id} से उठाव  ` : 'शायद उठाव शेष'}
+        {fps_id ? `चावल का ${fps_id} से उठाव  ` : 'शायद उठाव शेष'}
     </p>;
 }
 export default PortComponent;
